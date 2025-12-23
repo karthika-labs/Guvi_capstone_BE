4e46ef9d2b24aedb91ec678e891ae4d2a356a43d
Karthika K <karthikakarthikeyan0510@gmail.com>
2025-12-23 12:34:52 +0530

    all files completed

--- backend-avatar/models/favorites.js
+++
@@ -1,7 +0,0 @@
-const mongoose=require('mongoose')
-const favoriteSchema = new mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    recipeId:[ { type: mongoose.Schema.Types.ObjectId, ref: 'Recipe', required: true }]
-})
-
-module.exports = mongoose.model('Favorite', favoriteSchema)
--- backend/models/comments.js
+++
@@ -1,8 +0,0 @@
- const mongoose=require("mongoose")
- const commentsSchema=new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  recipeId: { type: mongoose.Schema.Types.ObjectId, ref: "Recipe" },
-      text: { type: String },
-     
- },{ timestamps: true})
-module.exports=mongoose.model("Comment",commentsSchema)
